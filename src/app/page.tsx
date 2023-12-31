102fef8fddabebd521f002439e0c48f1d216ef77
SEJ <jink6111@gmail.com>
2023-12-31 17:10:34 +0900

    create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,13 @@
+import StartButton from 'components/common/home/StartButton';
+
 /**
  * Home 페이지 컴포넌트
  */
 export default function HomePage() {
-  return <main className='w-full min-h-screen'></main>;
+  return (
+    <main className='w-full min-h-screen pt-12 flex flex-col justify-center items-center gap-y-5'>
+      <span className='text-3xl text-slate-600'>Hello. It's random quiz.</span>
+      <StartButton />
+    </main>
+  );
 }
---
+++ src/components/common/home/StartButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+/**
+ * 시작버튼 컴포넌트
+ */
+export default function StartButton() {
+  return (
+    <button className='p-2 bg-green-500 rounded-md text-lg text-white hover:-translate-y-0.5 hover:transition-transform'>
+      Start Quiz !
+    </button>
+  );
+}
